acdc46ee5c8f44acb08ee4e6e54f3c2f9f16472f
jimmy ibanez <jimmy.ibanez@outlook.com>
2018-03-14 18:58:32 +0000

    Integration of categories (Backoffice 2/5)

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,13 @@
 Rails.application.routes.draw do
-  namespace :backoffice do
-  get 'categories/index'
-  end
+  
+  get 'backoffice', to: 'backoffice/dashboard#index'
 
   namespace :site do
     get 'home', to: 'home#index'
   end
 
-  get 'backoffice', to: 'backoffice/dashboard#index'
-
   namespace :backoffice do
+    resources :categories, except: [:show, :destroy]
     get 'dashboard', to: 'dashboard#index'
   end
 
